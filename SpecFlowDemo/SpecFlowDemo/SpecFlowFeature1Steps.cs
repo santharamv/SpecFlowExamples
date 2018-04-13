2de06ca91190acd005c883cbced3cce7f0c412bb
Santharam Venkatapathy <santharam81@hotmail.com>
2018-04-13 21:54:21 +0100

    First set of SpecFlow examples

---
+++ SpecFlowDemo/SpecFlowDemo/SpecFlowFeature1Steps.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemo
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int result = 0;
+        private Calculator calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I also have entered (.*) into the calculator")]
+        public void GivenIAlsoHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result, "Total is not correct");
+            Console.Out.WriteLine("Test Passed");
+        }
+    }
+}
